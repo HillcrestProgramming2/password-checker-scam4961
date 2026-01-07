639312e8dd2c2d97d8e5f3e042a0b57018d8020c
Samc <samuelbrasileiro202@gmail.com>
2026-01-07 16:21:34 -0700

    FINISHED ALL THE TEST UML AND FLOWCHART

--- Password Checker App/src/test/java/org/hillcrest/chapter6/password/CriteriaCheckerTest.java
+++ Password Checker App/src/test/java/org/hillcrest/chapter6/password/CriteriaCheckerTest.java
@@ -1,9 +1,34 @@
 package org.hillcrest.chapter6.password;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+/**
+ * Tests possible outcomes to see if the prediction meets the actual output
+ * for the methods EvaluateCriteria & DetermineStrength
+ */
 public class CriteriaCheckerTest {
+    @Test
+    public void testEvaluateCriteria_shouldBe5__testEvaluateStrengthShouldBeStrong(){
+        int score = CriteriaChecker.evaluateCriteria("Secure@2024");
+        Assertions.assertEquals(5 ,score);
+        Assertions.assertEquals("Strong",CriteriaChecker.determineStrength(score));
 
-    public void test
+    }
+
+    @Test
+    public void testEvaluateCriteria_shouldBe3__testEvaluateStrengthShouldBeModerate(){
+        int score = CriteriaChecker.evaluateCriteria("password1234");
+        Assertions.assertEquals(3 ,score);
+        Assertions.assertEquals("Moderate",CriteriaChecker.determineStrength(score));
+    }
+    @Test
+    public void testEvaluateCriteria_shouldBe1__testEvaluateStrengthShouldBeWeak(){
+        int score = CriteriaChecker.evaluateCriteria("1234");
+        Assertions.assertEquals(1 ,score);
+        Assertions.assertEquals("Weak",CriteriaChecker.determineStrength(score));
+    }
+
+
 
 }
